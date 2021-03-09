9b16ec184046f697ff2b35de8702c8f825dd55b9
Chuck Schleutker <chuck.schleutker@gmail.com>
2021-03-09 03:25:42 -0500

    stuff

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-async function main() {
-  require("dotenv").config();
-  const { ChatClient } = require("dank-twitch-irc");
+function main() {
+  try {
+    require("dotenv").config();
+    const { ChatClient } = require("dank-twitch-irc");
 
-  const {
-    CHANNEL,
-    TWITCH_OAUTH_USERNAME, // this is your account id NOT display name; see https://github.com/robotty/dank-twitch-irc#usage
-    TWITCH_OAUTH_PASSWORD, // check out https://twitchapps.com/tmi/ for oauth key
-  } = process.env;
+    const {
+      CHANNEL,
+      TWITCH_OAUTH_USERNAME, // this is your account id NOT display name; see https://github.com/robotty/dank-twitch-irc#usage
+      TWITCH_OAUTH_PASSWORD, // check out https://twitchapps.com/tmi/ for oauth key
+    } = process.env;
 
-  const SOME_EDDIES =
-    "moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E  moon2E";
-  const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz0123456789";
-  const SLEEP_MS = 420;
+    const SOME_EDDIES =
+      "moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E moon2E  moon2E";
+    const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz0123456789";
+    const SLEEP_MS = 420;
 
-  let client = new ChatClient({
-    username: TWITCH_OAUTH_USERNAME,
-    password: TWITCH_OAUTH_PASSWORD,
-    ignoreUnhandledPromiseRejections: false,
-  });
-  client.connect();
-  client.join(CHANNEL);
-
-  function getRandomChar(str) {
-    return ALPHANUMERIC.charAt(Math.floor(Math.random() * ALPHANUMERIC.length));
-  }
-
-  async function sleep(ms = SLEEP_MS) {
-    return new Promise((resolve) => {
-      setTimeout(() => resolve(), ms);
+    let client = new ChatClient({
+      username: TWITCH_OAUTH_USERNAME,
+      password: TWITCH_OAUTH_PASSWORD,
+      ignoreUnhandledPromiseRejections: true,
     });
-  }
 
-  async function run() {
-    console.info(`running eddie on ${CHANNEL}...`);
-    while (true) {
-      await sleep();
-      await client.say(CHANNEL, `${SOME_EDDIES} ${getRandomChar()}`);
+    function getRandomChar() {
+      return ALPHANUMERIC.charAt(
+        Math.floor(Math.random() * ALPHANUMERIC.length)
+      );
     }
-  }
 
-  await run();
+    async function sleep(ms = SLEEP_MS) {
+      return new Promise((resolve) => {
+        setTimeout(() => resolve(), ms);
+      });
+    }
+
+    async function run() {
+      while (true) {
+        await sleep();
+        await client.say(CHANNEL, `${SOME_EDDIES} ${getRandomChar()}`);
+      }
+    }
+
+    client.connect();
+    client.join(CHANNEL);
+    client.on("ready", () => {
+      run().then(() => console.info(`Joined ${CHANNEL}`));
+    });
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-module.exports = (async () => {
-  try {
-    await main();
-  } catch (err) {
-    console.log(err.message);
-  }
-})();
+module.exports = {
+  default: main(),
+};
